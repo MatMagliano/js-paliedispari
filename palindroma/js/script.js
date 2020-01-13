288f87c952729ba4c4e41cd56a1c772f3f82bf9f
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-13 14:41:17 +0100

    inizio

---
+++ palindroma/js/script.js
@@ -0,0 +1,11 @@
+alert('Hello World')
+
+//---------------------------
+
+// Scrivi una funzione per capire se la parola è palindroma
+
+//---------------------------
+
+// Inserimento frase
+
+var parolaInserita = prompt('Inserisci una parola');
