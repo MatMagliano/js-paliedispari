412302e3218d21218aa13ce8457b31ea4b561511
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-13 16:40:21 +0100

    Inizio pari e dispari

---
+++ pari-dispari/js/script.js
@@ -0,0 +1,14 @@
+// alert('Hello World')
+
+// L’utente sceglie pari o dispari e un numero
+// da 1 a 5.
+// Generiamo un numero random (sempre da 1
+// a 5) per il computer.
+// Sommiamo i due numeri e dichiariamo chi ha
+// vinto.
+
+//------------SCELTA PARI O DISPARI------------------------------
+
+var pariDispari = prompt('Scegli tra pari e dispari');
+pariDispari = pariDispari.toLowerCase()
+console.log(pariDispari);
