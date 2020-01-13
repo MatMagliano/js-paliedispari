d4227cfa5c907f12339b56810ce239185cbeb65d
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-13 15:56:32 +0100

    controllo con while e for

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -13,14 +13,42 @@
 var parolaInserita = prompt('Inserisci una parola');
 console.log(parolaInserita);
 
-//----------------------------
-
-// Controllo palindroma
-
 var parolaPalindroma = '';
 
-for (var i = parolaInserita.length; i >= 0; i -- ) {
+//----------------------------
+
+// Controllo palindroma col for
+
+// for (var i = parolaInserita.length - 1; i >= 0; i -- ) {
+//   parolaPalindroma += parolaInserita[i];
+// }
+// console.log(parolaPalindroma);
+//
+// if (parolaInserita == parolaPalindroma) {
+//   console.log('la parola è palindroma!');
+// }
+// else {
+//   console.log('laparola non è palindroma');
+// }
+
+//----------------------------
+
+// Controllo palindroma col while
+
+var i = parolaInserita.length - 1;
+
+while (i >= 0) {
   parolaPalindroma += parolaInserita[i];
+  i --;
 }
 
 console.log(parolaPalindroma);
+
+if (parolaInserita == parolaPalindroma) {
+  console.log('la parola è palindroma!');
+}
+else {
+  console.log('laparola non è palindroma');
+}
+
+//----------------------------
