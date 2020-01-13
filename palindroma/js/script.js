5d2dbff4c59d210350e83ebcf416380c7670dd04
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-13 16:26:25 +0100

    fine esercizio palindroma

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -4,20 +4,15 @@
 
 // Scrivi una funzione per capire se la parola è palindroma
 
-//---------------------------
-
-// Inserimento frase
-
-
+//-----------Inserimento frase----------------
 
 var parolaInserita = prompt('Inserisci una parola');
+parolaInserita = parolaInserita.toLowerCase()
 console.log(parolaInserita);
 
-var parolaPalindroma = '';
 
-//----------------------------
+//------------Controllo palindroma col for----------------
 
-// Controllo palindroma col for
 
 // for (var i = parolaInserita.length - 1; i >= 0; i -- ) {
 //   parolaPalindroma += parolaInserita[i];
@@ -31,24 +26,53 @@ var parolaPalindroma = '';
 //   console.log('laparola non è palindroma');
 // }
 
-//----------------------------
+//----------Controllo palindroma col while------------------
 
-// Controllo palindroma col while
 
-var i = parolaInserita.length - 1;
+// var i = parolaInserita.length - 1;
+//
+// while (i >= 0) {
+//   parolaPalindroma += parolaInserita[i];
+//   i --;
+// }
+//
+// console.log(parolaPalindroma);
+//
+// if (parolaInserita == parolaPalindroma) {
+//   console.log('la parola è palindroma!');
+// }
+// else {
+//   console.log('laparola non è palindroma');
+// }
 
-while (i >= 0) {
-  parolaPalindroma += parolaInserita[i];
-  i --;
+//--------- FUNZIONE -------------------
+
+var controllo = palindrome(parolaInserita)
+
+if (controllo == true) {
+  console.log('La parola ' + parolaInserita + ' è palindroma!');
+} else {
+   console.log('La parola ' + parolaInserita + ' non è palindroma!');
 }
 
-console.log(parolaPalindroma);
 
-if (parolaInserita == parolaPalindroma) {
-  console.log('la parola è palindroma!');
-}
-else {
-  console.log('laparola non è palindroma');
-}
 
-//----------------------------
+function palindrome(controlloParola) {
+  var parolaPalindroma = '';
+
+  for (var i = parolaInserita.length - 1; i >= 0; i -- ) {
+    parolaPalindroma += parolaInserita[i];
+  }
+
+  if (parolaInserita == parolaPalindroma) {
+
+    parolaPalindroma = true;
+
+  } else {
+
+    parolaPalindroma = false;
+
+  }
+
+  return parolaPalindroma;
+}
